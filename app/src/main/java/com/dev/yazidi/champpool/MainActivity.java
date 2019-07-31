343de57dc0374278dc080c6580aa7f3bd7c9fb1b
Rasonakima <Rasonakima@gmail.com>
2019-07-31 17:06:33 +0000

    Added pool list countainer (gridveiw)

--- app/src/main/java/com/dev/yazidi/champpool/MainActivity.java
+++ app/src/main/java/com/dev/yazidi/champpool/MainActivity.java
@@ -2,6 +2,12 @@ package com.dev.yazidi.champpool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        List<String> lanes = new ArrayList<String>();
+        GridView gridViewHeader = (GridView) findViewById(R.id.poolListHeader);
+        gridViewHeader.setAdapter(new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,lanes));
     }
 }
